ca48c0b5851efbbb0d31f150430a52fe53abdf10
Ziyang Li <ZL@auburn.edu>
2020-07-12 03:53:45 -0500

    Updates

--- precache-manifest.f0c90e31a75dd8fad583d991e9861b70.js
+++ precache-manifest.f0c90e31a75dd8fad583d991e9861b70.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4b6825f2241305177fd775280dbf23f8",
+    "revision": "5c750f4c7ee24651f96f3f09fae4e641",
     "url": "/index.html"
   },
   {
-    "revision": "2e01cf62123a0f8c9568",
+    "revision": "e0f0f51ad847fddf336f",
     "url": "/static/css/2.f681e662.chunk.css"
   },
   {
-    "revision": "458ca121336ddc086a46",
+    "revision": "16e124bf01a18b088bb5",
     "url": "/static/css/main.355c14a6.chunk.css"
   },
   {
-    "revision": "2e01cf62123a0f8c9568",
-    "url": "/static/js/2.5d61899d.chunk.js"
+    "revision": "e0f0f51ad847fddf336f",
+    "url": "/static/js/2.cc833293.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/static/js/2.5d61899d.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.cc833293.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "458ca121336ddc086a46",
-    "url": "/static/js/main.60291b6f.chunk.js"
+    "revision": "16e124bf01a18b088bb5",
+    "url": "/static/js/main.f1828a23.chunk.js"
   },
   {
     "revision": "9467c73a2f1397f3fdb7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2682fed4264d4be24d75bb80f1776178.js"
+  "/precache-manifest.f0c90e31a75dd8fad583d991e9861b70.js"
 );
 
 self.addEventListener('message', (event) => {
