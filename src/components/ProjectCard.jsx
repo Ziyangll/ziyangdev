4ff0a522b32e9c64faab8b1060f5fc6463b79d33
Ziyang Li <ZL@auburn.edu>
2020-07-12 03:53:13 -0500

    refactored project and cards

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "../styles/styles.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import IconButton from "@material-ui/core/IconButton";
+import CardActions from "@material-ui/core/CardActions";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+import CodeIcon from "@material-ui/icons/Code";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 300,
+    maxWidth: 300,
+    margin: "3rem",
+  }
+});
+
+export default function ProjectCard( { websiteLink, GitHubLink, imgPath, alt, title, subtitle,} ) {
+  const classes = useStyles();
+
+    return(
+      <Card className={classes.root + " grow"}>
+        <CardActionArea
+          onClick={() =>
+              window.location.href = {websiteLink}
+          }>
+          <CardMedia
+            component='img'
+            alt={alt}
+            height='140'
+            image={process.env.PUBLIC_URL + imgPath}
+            title={title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant='h5' component='h2'>
+                {title}
+            </Typography>
+            <Typography variant='body2' color='textSecondary' component='p'>
+                {subtitle}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+            <IconButton aria-label="GitHub" style={{marginLeft: "auto"}} 
+                onClick={() =>
+                    window.location.href = {GitHubLink}
+                }>
+                <CodeIcon />
+            </IconButton>
+        </CardActions>
+      </Card>
+    );
+}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import "../styles/styles.css";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
+import IconButton from "@material-ui/core/IconButton";
+import CardActions from "@material-ui/core/CardActions";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 
+import CodeIcon from "@material-ui/icons/Code";
+
+import ProjectCard from "./ProjectCard.jsx";
+
 const useStyles = makeStyles({
-  root: {
-    minWidth: 300,
-    maxWidth: 300,
-    margin: "3rem",
-  },
   cards: {
     display: "flex",
     flexWrap: "wrap",
@@ -26,138 +27,54 @@ export default function Projects() {
 
   return (
     <div className={classes.cards}>
-      <Card className={classes.root + " grow"}>
-        <CardActionArea
-          onClick={() =>
-            window.location.replace("https://github.com/Ziyangll")
-          }>
-          <CardMedia
-            component='img'
-            alt='project 1'
-            height='140'
-            image={process.env.PUBLIC_URL + "/projects-images/blob.svg"}
-            title='project 1'
-          />
-          <CardContent>
-            <Typography gutterBottom variant='h5' component='h2'>
-              project 1
-            </Typography>
-            <Typography variant='body2' color='textSecondary' component='p'>
-              project 1
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className={classes.root + " grow"}>
-        <CardActionArea
-          onClick={() =>
-            window.location.replace("https://github.com/Ziyangll")
-          }>
-          <CardMedia
-            component='img'
-            alt='project 2'
-            height='140'
-            image={process.env.PUBLIC_URL + "/projects-images/blob2.svg"}
-            title='project 2'
-          />
-          <CardContent>
-            <Typography gutterBottom variant='h5' component='h2'>
-              project 2
-            </Typography>
-            <Typography variant='body2' color='textSecondary' component='p'>
-              project 2
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className={classes.root + " grow"}>
-        <CardActionArea
-          onClick={() =>
-            window.location.replace("https://github.com/Ziyangll")
-          }>
-          <CardMedia
-            component='img'
-            alt='project 3'
-            height='140'
-            image={process.env.PUBLIC_URL + "/projects-images/blob3.svg"}
-            title='project 3'
-          />
-          <CardContent>
-            <Typography gutterBottom variant='h5' component='h2'>
-              project 3
-            </Typography>
-            <Typography variant='body2' color='textSecondary' component='p'>
-              project 3
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className={classes.root + " grow"}>
-        <CardActionArea
-          onClick={() =>
-            window.location.replace("https://github.com/Ziyangll")
-          }>
-          <CardMedia
-            component='img'
-            alt='project 4'
-            height='140'
-            image={process.env.PUBLIC_URL + "/projects-images/blob4.svg"}
-            title='project 4'
-          />
-          <CardContent>
-            <Typography gutterBottom variant='h5' component='h2'>
-              project 4
-            </Typography>
-            <Typography variant='body2' color='textSecondary' component='p'>
-              project 4
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className={classes.root + " grow"}>
-        <CardActionArea
-          onClick={() =>
-            window.location.replace("https://github.com/Ziyangll")
-          }>
-          <CardMedia
-            component='img'
-            alt='project 5'
-            height='140'
-            image={process.env.PUBLIC_URL + "/projects-images/blob5.svg"}
-            title='project 5'
-          />
-          <CardContent>
-            <Typography gutterBottom variant='h5' component='h2'>
-              project 5
-            </Typography>
-            <Typography variant='body2' color='textSecondary' component='p'>
-              project 5
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-      <Card className={classes.root + " grow"}>
-        <CardActionArea
-          onClick={() =>
-            window.location.replace("https://github.com/Ziyangll")
-          }>
-          <CardMedia
-            component='img'
-            alt='project 6'
-            height='140'
-            image={process.env.PUBLIC_URL + "/projects-images/blob6.svg"}
-            title='project 6'
-          />
-          <CardContent>
-            <Typography gutterBottom variant='h5' component='h2'>
-              project 6
-            </Typography>
-            <Typography variant='body2' color='textSecondary' component='p'>
-              project 6
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <ProjectCard  
+        websiteLink = "https://github.com/Ziyangll" 
+        GitHubLink = "https://github.com/Ziyangll" 
+        imgPath = "/projects-images/blob.svg" 
+        alt = "project 1" 
+        title = "placeholder" 
+        subtitle = "project 1"
+      />
+      <ProjectCard  
+        websiteLink = "https://github.com/Ziyangll" 
+        GitHubLink = "https://github.com/Ziyangll" 
+        imgPath = "/projects-images/blob2.svg" 
+        alt = "project 2" 
+        title = "placeholder" 
+        subtitle = "project 2"
+      />
+      <ProjectCard  
+        websiteLink = "https://github.com/Ziyangll" 
+        GitHubLink = "https://github.com/Ziyangll" 
+        imgPath = "/projects-images/blob3.svg" 
+        alt = "project 3" 
+        title = "placeholder" 
+        subtitle = "project 3"
+      />
+      <ProjectCard  
+        websiteLink = "https://github.com/Ziyangll" 
+        GitHubLink = "https://github.com/Ziyangll" 
+        imgPath = "/projects-images/blob4.svg" 
+        alt = "project 4" 
+        title = "placeholder" 
+        subtitle = "project 4"
+      />
+      <ProjectCard  
+        websiteLink = "https://github.com/Ziyangll" 
+        GitHubLink = "https://github.com/Ziyangll" 
+        imgPath = "/projects-images/blob5.svg" 
+        alt = "project 5" 
+        title = "placeholder" 
+        subtitle = "project 5"
+      />
+      <ProjectCard  
+        websiteLink = "https://github.com/Ziyangll" 
+        GitHubLink = "https://github.com/Ziyangll" 
+        imgPath = "/projects-images/blob6.svg" 
+        alt = "project 6" 
+        title = "placeholder" 
+        subtitle = "project 6"
+      />
     </div>
   );
 }
