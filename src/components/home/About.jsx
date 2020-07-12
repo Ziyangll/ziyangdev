d96797b322fc5864aa9c7761c15227ddc8a09b8c
Ziyang Li <ZL@auburn.edu>
2020-07-12 03:01:20 -0500

    remove some noise

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
         <h1 class="title-text">Introduction</h1>
         <h3 class="text">I am a 19 years old student studying <span class="title-text">Software Engineering</span></h3>
         <h3 class="text"><span class="title-text">Fullstack</span> web and mobile developer</h3>
-        <h3 class="text">MERN stack <span class="title-text">Mongodb Express <span className="persona-text-two">React</span> Node.js</span>
+        <h3 class="text">MERN stack <span class="title-text">Mongodb Express <span>React</span> Node.js</span>
         </h3>
         <h3 class="text"><span class="title-text">Google Cloud Platform</span>, AWS</h3>
         <h3 class="text">Swift, <span class="title-text">React-Native</span></h3>
