788b3c7292a4decf79af378ed8391ca9a635990f
Ziyang Li <ZL@auburn.edu>
2020-07-14 21:08:08 -0500

    fix about

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -9,11 +9,9 @@ export default function About() {
         <h3 class="text"><span class="title-text">Fullstack</span> web and mobile developer</h3>
         <h3 class="text">MERN stack <span class="title-text">Mongodb Express <span>React</span> Node.js</span>
         </h3>
-        <h3 class="text"><span>Google Cloud Platform</span>, AWS</h3>
-        <h3 class="text">Swift, <span>React-Native</span></h3>
         <br/>
         <h1 class="text">Programming Languages</h1>
-        <h3 class="title-text">JavaScript, Python, C++, Java, and Swift</h3>
+        <h3 class="title-text">JavaScript, Python, C++, C#, Java, and Swift</h3>
         <br></br>
     </div>
   );
