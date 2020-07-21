5ad8f684708dbd2aeaea8f95681a0429928fa1fa
Ziyang Li <ZL@auburn.edu>
2020-07-21 06:23:10 -0500

    added icons for my projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,42 +21,42 @@ export default function Projects() {
       <ProjectCard
         websiteLink='https://ziyang.dev'
         GitHubLink='https://github.com/Ziyangll/ziyangdev'
-        imgPath='/projects-images/blob6.svg'
+        imgPath='/projects-images/Z.svg'
         title='Ziyang.dev'
         subtitle='React'
       />
       <ProjectCard
         websiteLink='https://todos-283807.uc.r.appspot.com/'
         GitHubLink='https://github.com/Ziyangll/Todos'
-        imgPath='/projects-images/blob.svg'
+        imgPath='/projects-images/Todo.svg'
         title='Todolist'
         subtitle='MongoDB Express React Node Google-Cloud'
       />
       <ProjectCard
         websiteLink='https://ziyangll.github.io/GoldenGoose/'
         GitHubLink='https://github.com/Ziyangll/GoldenGoose'
-        imgPath='/projects-images/blob2.svg'
+        imgPath='/projects-images/goose.png'
         title='Golden Goose'
         subtitle='Flask MongoDB Bootstrap4'
       />
       <ProjectCard
         websiteLink='https://valorantlineups.online/'
         GitHubLink='https://github.com/Ziyangll/valorant-lineups'
-        imgPath='/projects-images/blob3.svg'
+        imgPath='/projects-images/valorant.svg'
         title='Valorant Lineups'
         subtitle='React'
       />
       <ProjectCard
         websiteLink='https://github.com/Ziyangll/trekking'
         GitHubLink='https://github.com/Ziyangll/trekking'
-        imgPath='/projects-images/blob4.svg'
+        imgPath='/projects-images/trekking.webp'
         title='Trekking New Space'
         subtitle='React'
       />
       <ProjectCard
         websiteLink='https://ziyangll.github.io/DuoQ/'
         GitHubLink='https://github.com/Ziyangll/DuoQ'
-        imgPath='/projects-images/blob5.svg'
+        imgPath='/projects-images/blob.svg'
         title='Duo Q'
         subtitle='HTML CSS'
       />
