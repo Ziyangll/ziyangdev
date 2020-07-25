df61ec0b94f8600363d00427272397e7fc6d77ce
Ziyang Li <ZL@auburn.edu>
2020-07-25 02:56:49 -0500

    Revert "sick particles"
    This reverts commit d50c56faed8f4b741bd5cf30f31e9d82ce9bb53a.

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -1,45 +1,21 @@
 import React from "react";
 import "../../styles/styles.css";
 import FadeInSection from "../FadeInSection";
-import Particles from "react-particles-js";
 
 export default function Intro() {
   return (
     <div className='content'>
       <div className='title'>
         <FadeInSection key='title'>
-          <h1 class='title-text'>Ziyang Li</h1>
-          <h1 class='text subtitle'>Fullstack Developer</h1>
+          <h1 class="title-text">Ziyang Li</h1>
+          <h1 class="text subtitle">fullstack developer</h1>
         </FadeInSection>
       </div>
-
-      <Particles
-        className='intro-background'
-        params={{
-          particles: {
-            number: {
-              density: {
-                enable: true,
-              },
-            },
-            size: {
-              value: 0,
-            },
-            links: {
-              distance: 200,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              enable: true,
-              speed: 2,
-            },
-            fpsLimit: 60,
-            detectRetina: true,
-          },
-        }}
-      />
+      <img
+        className='IntroImage'
+        src={process.env.PUBLIC_URL + "/nice.svg"}
+        alt='ziyang li'>
+      </img>
     </div>
   );
 }
