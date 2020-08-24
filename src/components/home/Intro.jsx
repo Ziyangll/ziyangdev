3fb64a6ff939d49bf1e467428f88947a4a5598ce
Ziyang Li <ZL@auburn.edu>
2020-08-24 11:29:58 -0500

    stop moving the big intro text

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "../../styles/styles.css";
-import FadeInSection from "../FadeInSection";
 import Particles from "react-particles-js";
 
 export default function Intro() {
   return (
     <div className='content'>
       <div className='title'>
-        <FadeInSection key='title'>
           <h1 class='title-text'>Ziyang Li</h1>
-          <h1 class='text subtitle'>Fullstack Developer</h1>
-        </FadeInSection>
+          <h1 class='text subtitle'>Software Engineer</h1>
       </div>
 
       <Particles
