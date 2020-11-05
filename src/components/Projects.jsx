acfc51711492a62decb6872f47668daa0e50db98
Ziyang Li <zl@auburn.edu>
2020-11-04 16:27:08 -0800

    resume changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,49 +17,56 @@ export default function Projects() {
   const classes = useStyles();
 
   return (
-    <div className={classes.cards}>
-      <ProjectCard
-        websiteLink='https://ziyang.dev'
-        GitHubLink='https://github.com/Ziyangll/ziyangdev'
-        imgPath='/projects-images/Z.svg'
-        title='Ziyang.dev'
-        subtitle='React'
-      />
-      <ProjectCard
-        websiteLink='https://todos-283807.uc.r.appspot.com/'
-        GitHubLink='https://github.com/Ziyangll/Todos'
-        imgPath='/projects-images/Todo.svg'
-        title='Todolist'
-        subtitle='MongoDB Express React Node Google-Cloud'
-      />
-      <ProjectCard
-        websiteLink='https://ziyangll.github.io/GoldenGoose/'
-        GitHubLink='https://github.com/Ziyangll/GoldenGoose'
-        imgPath='/projects-images/goose.png'
-        title='Golden Goose'
-        subtitle='Flask MongoDB Bootstrap4'
-      />
-      <ProjectCard
-        websiteLink='https://valorantlineups.online/'
-        GitHubLink='https://github.com/Ziyangll/valorant-lineups'
-        imgPath='/projects-images/valorant.svg'
-        title='Valorant Lineups'
-        subtitle='React'
-      />
-      <ProjectCard
-        websiteLink='https://github.com/Ziyangll/trekking'
-        GitHubLink='https://github.com/Ziyangll/trekking'
-        imgPath='/projects-images/trekking.webp'
-        title='Trekking New Space'
-        subtitle='React'
-      />
-      <ProjectCard
-        websiteLink='https://ziyangll.github.io/DuoQ/'
-        GitHubLink='https://github.com/Ziyangll/DuoQ'
-        imgPath='/projects-images/blob.svg'
-        title='Duo Q'
-        subtitle='HTML CSS'
-      />
-    </div>
-  );
+		<div className={classes.cards}>
+			<ProjectCard
+				websiteLink='https://ziyang.dev'
+				GitHubLink='https://github.com/Ziyangll/ziyangdev'
+				imgPath='/projects-images/Z.svg'
+				title='Ziyang.dev'
+				subtitle='React'
+			/>
+			<ProjectCard
+				websiteLink='https://todos-283807.uc.r.appspot.com/'
+				GitHubLink='https://github.com/Ziyangll/Todos'
+				imgPath='/projects-images/Todo.svg'
+				title='Todolist'
+				subtitle='MongoDB Express React Node Google-Cloud'
+			/>
+			<ProjectCard
+				websiteLink='https://acm-auburn.vercel.app/'
+				GitHubLink='https://github.com/Ziyangll/ACM-Auburn'
+				imgPath='/projects-images/blob2.svg'
+				title='ACM Auburn website'
+				subtitle='Next.js'
+			/>
+			<ProjectCard
+				websiteLink='https://ziyangll.github.io/GoldenGoose/'
+				GitHubLink='https://github.com/Ziyangll/GoldenGoose'
+				imgPath='/projects-images/goose.png'
+				title='Golden Goose'
+				subtitle='Flask MongoDB Bootstrap4'
+			/>
+			<ProjectCard
+				websiteLink='https://valorantlineups.online/'
+				GitHubLink='https://github.com/Ziyangll/valorant-lineups'
+				imgPath='/projects-images/valorant.svg'
+				title='Valorant Lineups'
+				subtitle='React'
+			/>
+			<ProjectCard
+				websiteLink='https://github.com/Ziyangll/trekking'
+				GitHubLink='https://github.com/Ziyangll/trekking'
+				imgPath='/projects-images/trekking.webp'
+				title='Trekking New Space'
+				subtitle='React'
+			/>
+			<ProjectCard
+				websiteLink='https://ziyangll.github.io/DuoQ/'
+				GitHubLink='https://github.com/Ziyangll/DuoQ'
+				imgPath='/projects-images/blob.svg'
+				title='Duo Q'
+				subtitle='HTML CSS'
+			/>
+		</div>
+	)
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import { ThemeProvider, Button } from "@material-ui/core";
-import GetAppIcon from "@material-ui/icons/GetApp";
-import "../styles/styles.css";
-import theme from "../styles/theme";
+import React from 'react'
+import { ThemeProvider, Button } from '@material-ui/core'
+import GetAppIcon from '@material-ui/icons/GetApp'
+import '../styles/styles.css'
+import theme from '../styles/theme'
 
 function Resume() {
-  return (
-    <div className='main iframe'>
-      <div className='download-button'>
-        <ThemeProvider theme={theme}>
-          <Button
-            variant='contained'
-            startIcon={<GetAppIcon />}
-            href='https://docs.google.com/document/d/16LwQznWREwZ1q91mL66XC-1dowZzPzUMwREGq1g3HqQ/edit?usp=sharing'>
-            Download
-          </Button>
-        </ThemeProvider>
-      </div>
+	return (
+		<div className='main iframe'>
+			<div className='download-button'>
+				<ThemeProvider theme={theme}>
+					<Button
+						variant='contained'
+						startIcon={<GetAppIcon />}
+						href='https://docs.google.com/document/d/1aHJuCZOCFbpmeYudKQWdWOiMXtNY0Fp8X9pUo-5hDas/edit?usp=sharing'>
+						Download
+					</Button>
+				</ThemeProvider>
+			</div>
 
-      <iframe
-        title='resume'
-        className='resume'
-        src='https://docs.google.com/document/d/e/2PACX-1vQZfX3KiO4G7XFmK32wZBl8-viS113duNsh-GohyksdcMGlpeaoQFnC3RWEPa-zBM5yl1kVO3tx38Bl/pub?embedded=true'>
-        Your browser doesn't support iframes
-      </iframe>
-    </div>
-  );
+			<iframe
+				title='resume'
+				className='resume'
+				src='https://docs.google.com/document/d/e/2PACX-1vRJAK5l6JkhtPKW0hojDVVfRXbws6cs0aVHC3ZI1GoTuJho6nYSY53Y0CbzdDoPyO0c0VutEl8PWryF/pub?embedded=true'>
+				Your browser doesn't support iframes
+			</iframe>
+		</div>
+	)
 }
 
-export default Resume;
+export default Resume
